3fc0ac03f484af4d8295b42e6d7a0cf2f39c4504
harshbatham15 <119497494+harshbatham15@users.noreply.github.com>
2022-12-25 12:02:20 +0530

    Add files via upload

---
+++ prime_number.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	//prime numbers
+	int n,i,d;
+	bool c=true;
+	cout<<"Enter number: ";
+	cin>>n;
+	i=2;
+	while(i<n)
+	{
+       d=n%i;
+       if (d==0)
+       {
+       	c=false;
+       	break;
+       }
+	}
+
+	if (c)
+		cout<<"Prime Number"<<endl;
+	else
+		cout<<"Not Prime"<<endl;
+	return 0;
+}
